f612d03933b1e3449919f21eb33d926773199cef
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2022-02-25 17:20:21 -0300

    vai toma no cu

---
+++ src/modules/product.js
@@ -0,0 +1,18 @@
+
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const produto = new Schema({
+  petshop_id: {
+    type: Schema.Types.ObjectId,
+    ref: "Petshop"
+  },
+  nome: String,
+  capa: String,
+  preco: Number,
+  avaliacoes: Number
+});
+
+module.exports = mongoose.model("produto", petshop);
