c73a4e186cc7b4305a11943fb5c0be3668c6f92e
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2022-02-25 19:27:48 -0300

    aaaaa

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -2,13 +2,30 @@ const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async(req, res, next) =>{
+const Petshop = require('../modules/petshop')
+const product = require('../models/product')
+
+//const createSplitTransaction = require(injetar o pagar-me)
+router.get('/petshops/:id', async(req, res, next) =>{
 
 try {
-    res.json({message: false, message: "Teste"});
+    const petshops = await Petshop.findById(req.params.id);
+    let products = await product.find({
+        petshop_id: petshop._id});
+
+        res.json({ error: false, petshop: {...petshop._doc, products}})
+    res.json({error: false, petshops});
 } catch (error) {
     res.json({ error:true , message: error.message});
 }
-})
+});
+router.post('/purchase', async(req, res) => {
+    try {
+        const transaction = await createSplitTransaction(req.body)
+        res.json(transaction);
+    } catch (error) {
+        res.json({ error:true , message: error.message});
+}
+    })
 
 module.exports = router;
