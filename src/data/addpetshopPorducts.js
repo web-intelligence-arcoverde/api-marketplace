468efe7a6a89bf959471814473f2d728e03c71ea
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2022-02-25 09:13:58 -0300

    nodemonio

---
+++ src/data/addpetshopPorducts.js
@@ -0,0 +1,24 @@
+const Petshop = require('../modules/petshop');
+const Product = require ('../modules/product');
+const petshops = require('./petfood.json');
+//db 
+require('../db');
+
+const addPetshopsAndProducts = async () =>{
+
+try {
+    for (const petshop of petshops){
+        const newPetshop = await new Petshop(petshop).save();
+           await Product.insertMany(
+        petshop.produtos.map((p) => ({ ...p, petshop_id: newPetshop.petshop._id}))
+    ); 
+     }
+     console.log('Final Script final de teste');
+    
+} catch (error) {
+    console.log(error.message);
+}
+
+}
+
+addPetshopsAndProducts();
