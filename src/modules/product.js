cff2615ae1d55ae513d7120a17d18edbc2827d2f
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2022-03-01 18:55:26 -0300

    migrationmelhorada
    atttt

--- src/modules/product.js
+++ src/modules/product.js
@@ -1,18 +1,55 @@
 
 
+const { ObjectId } = require("bson");
 const mongoose = require("mongoose");
+const petshop = require("./petshop");
 
-const Schema = mongoose.Schema;
+//const Schema = mongoose.Schema;
 
+///const produto = new Schema({
+//petshop_id: {
+//type: Schema.Types.ObjectId,
+//ref: "Petshop"
+//},
+//nome: String,
+//capa: String,
+//preco: Number,
+//avaliacoes: Number
+//});
+
+//module.exports = mongoose.model("produto", petshop);
+////
+const Schema = mongoose.Schema
 const product = new Schema({
+
+  nome: {
+
+    type: String
+  },
   petshop_id: {
     type: Schema.Types.ObjectId,
-    ref: "Petshop"
-  },
-  nome: String,
-  capa: String,
-  preco: Number,
-  avaliacoes: Number
-});
+    ref: 'Petshop',
 
-module.exports = mongoose.model("Product", petshop);
+  },
+  preco: {
+    type: Number
+  },
+  capa: {
+
+    type: String
+  },
+  preco: {
+
+    type: Number
+  },
+  CreatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  avaliacoes: {
+
+    type: Number
+  }
+});
+module.exports = mongoose.model("Product", product);
+//const User = mongoose.model('product', petshop);
