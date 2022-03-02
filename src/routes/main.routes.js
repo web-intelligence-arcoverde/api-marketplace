448e38a6e3dbc21cbc4e28a1facb971238137f2e
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2022-03-01 21:12:46 -0300

    addd

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -7,19 +7,31 @@ const Petshop = require('../modules/petshop')
 const product = require('../modules/product')
 
 //const createSplitTransaction = require(injetar o pagar-me)
-router.get('/petshops/:id', async(req, res, next) =>{
+router.get('/petshops', async(req, res, next) =>{
 
 try {
-    const petshops = await Petshop.findById(req.params.id);
-    let products = await product.find({
-        petshop_id: petshop._id});
-
-        res.json({ error: false, petshop: {...petshop._doc, products}})
+    const petshops = await Petshop.find();
     res.json({error: false, petshops});
+
 } catch (error) {
     res.json({ error:true , message: error.message});
 }
 });
+
+router.get('/petshops/:id', async(req, res, next) =>{
+
+    try {
+        const petshop = await Petshop.findById(req.params.id);
+        let products = await product.find({
+            petshop_id: petshop._id 
+        })
+        res.json({error: false, petshop:{...Petshop._doc, products}});
+    
+    } catch (error) {
+        res.json({ error:true , message: error.message});
+    }
+    });
+
 router.post('/purchase', async(req, res) => {
     try {
         const transaction = await createSplitTransaction(req.body)
