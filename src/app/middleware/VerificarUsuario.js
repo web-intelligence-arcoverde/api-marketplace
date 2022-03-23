c440343f33f9ead724fee1a6abbff6ed5ce26224
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2022-03-23 13:58:24 -0300

    Update VerificarUsuario.js

--- src/app/middleware/VerificarUsuario.js
+++ src/app/middleware/VerificarUsuario.js
@@ -31,22 +31,7 @@ checkDuplicateUsernameOrEmail = (req, res, next) => {
     });
   });
 };
-
-checkRolesExisted = (req, res, next) => {
-  if (req.body.roles) {
-    for (let i = 0; i < req.body.roles.length; i++) {
-      if (!ROLES.includes(req.body.roles[i])) {
-        res.status(400).send({
-          message: `Falhou! este cargo ${req.body.roles[i]} não existe!`,
-        });
-        return;
-      }
-    }
-  }
-  next();
-};
 const verifySignUp = {
-  checkDuplicateUsernameOrEmail,
-  checkRolesExisted,
+  checkDuplicateUsernameOrEmail
 };
 module.exports = verifySignUp;
