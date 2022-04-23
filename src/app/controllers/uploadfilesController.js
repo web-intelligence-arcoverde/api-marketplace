bae3acf1eb47f6a19c4f726c5b09c7c55628a3e5
RawdneyGoncalves <mendesrawdney@gmail.com>
2022-04-23 19:02:02 -0300

    Initial commit uploadImage

---
+++ src/app/controllers/uploadfilesController.js
@@ -0,0 +1,8 @@
+const AWS = require('aws-sdk');
+
+// Enter copied or downloaded access ID and secret key here
+const ID = '';
+const SECRET = '';
+
+// The name of the bucket that you have created
+const BUCKET_NAME = '';
