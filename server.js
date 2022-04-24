1cb8c50d641d4fc71db3e7a8577a44ec68c8ceb4
RawdneyGoncalves <mendesrawdney@gmail.com>
2022-04-23 23:53:28 -0300

    organization path files sucessed

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 
 const roleRoutes = require('./src/routes/role.routes');
@@ -10,49 +9,7 @@ const categoryRoutes = require('./src/routes/category.routes');
 const productRoutes = require('./src/routes/product.routes');
 const orderRoutes = require('./src/routes/order.routes');
 
-const app = express();
-
-const cors = require('cors');
-
-app.use(express.json());
-
-const morgan = require('morgan');
-
-var corsOptions = {
-    origin: 'http://localhost:8040',
-};
-
-const PORT = process.env.PORT || 8040;
-
-app.use(cors(corsOptions));
-
-app.use(express.urlencoded({ extended: true }));
-
-require('./src/app/database/database');
-
-app.use(morgan('dev'));
-app.use('/v1/api', roleRoutes);
-app.use('/v1/api', userRoutes);
-app.use('/v1/api', addressRoutes);
-app.use('/v1/api', marketRoutes);
-app.use('/v1/api', categoryRoutes);
-app.use('/v1/api', productRoutes);
-app.use('/v1/api/', orderRoutes);
-
-app.listen(PORT, () => {
-    console.log(`SERVIDOR RODANDO NA PORTA ${PORT}`);
-});
-=======
-const express = require('express');
-
-const roleRoutes = require('./src/routes/role.routes');
-const userRoutes = require('./src/routes/user.routes');
-const addressRoutes = require('./src/routes/address.routes');
-const marketRoutes = require('./src/routes/market.routes');
-
-const categoryRoutes = require('./src/routes/category.routes');
-const productRoutes = require('./src/routes/product.routes');
-const orderRoutes = require('./src/routes/order.routes');
+const updateFile = require('./src/routes/uploadfiles.routes')
 
 const app = express();
 
@@ -63,10 +20,10 @@ app.use(express.json());
 const morgan = require('morgan');
 
 var corsOptions = {
-  origin: 'http://localhost:8040',
+  origin: 'http://localhost:8050',
 };
 
-const PORT = process.env.PORT || 8040;
+const PORT = process.env.PORT || 8050;
 
 app.use(cors(corsOptions));
 
@@ -82,8 +39,8 @@ app.use('/v1/api', marketRoutes);
 app.use('/v1/api', categoryRoutes);
 app.use('/v1/api', productRoutes);
 app.use('/v1/api/', orderRoutes);
+app.use('/v1/api', updateFile)
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
->>>>>>> 17818472bc30a5a7e3e8365e77cb1f275492d072
--- src/routes/uploadfiles.routes.js
+++ src/routes/uploadfiles.routes.js
@@ -1,22 +1,15 @@
-const express = require("express");
+const {
+  updatefile
+} = require('../app/controllers/UploadfilesController');
+
+const express = require('express');
 const router = express.Router();
-const upload = require("../app/config/common");
-const { uploadFile } = require("../app/controllers/UploadfilesController");
-const fs = require("fs");
-const util = require("util");
-const unlinkFile = util.promisify(fs.unlink);
-router.post("/single", upload.single("image"), async (req, res) => {
-  console.log(req.file);
-  // uploading to AWS S3
-  const result = await uploadFile(req.file);  // Calling above function in s3.js
-  console.log("S3 response", result);
-  // You may apply filter, resize image before sending to client
-  // Deleting from local if uploaded in S3 bucket
-  await unlinkFile(req.file.path);
-  res.send({
-    status: "success",
-    message: "Arquivo Subido com sucesso!",
-    data: req.file,
-  });
-});
+
+router.route('/update').post(updatefile);
+//router.route('/address').get(readAddress);
+
+//router.route('/address/:id').delete(deleteAddress);
+//router.route('/address/:id').put(updateAddress);
+//router.route('/address/:id').get(findAddressById);
+
 module.exports = router;
